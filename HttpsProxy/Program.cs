8652e8b64c8e554f4ff9edff88c09d5b4cc7c712
Ivony <ivony@live.com>
2024-02-07 16:09:38 +0800

    添加项目文件。

---
+++ HttpsProxy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebHost.CreateDefaultBuilder();
+
+builder.UseKestrel( kestrel => kestrel.ListenLocalhost( 5000, options => options.UseConnectionLogging() ) );
+builder.ConfigureServices( services =>
+{
+
+  services.AddLogging();
+  services.AddHttpLogging( options => options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All );
+
+
+} );
+builder.Configure( builder =>
+{
+
+  builder.UseHttpLogging();
+
+
+} );
+var host = builder.Build();
+await host.RunAsync();
+
+
